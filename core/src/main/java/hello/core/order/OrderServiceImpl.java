c2cbc0f770e56fbced749ecec7e117daad13654a
leedongyeop <ldy_1204@naver.com>
2022-12-08 20:09:51 +0900

    롬복과 최신 트렌드

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,36 +3,15 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
-
-    /*
-    @Autowired private MemberRepository memberRepository;
-    필드로 의존관계 주입 -> 비추천 방식. 외부에서 테스트하기가 힘듬.
-    + NPE로 인해 결국 setter를 만들게 함 -> 이럴거면 setter injection이 낫다.
-     */
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    /* @Autowired(required = false) //주입할 대상이 없어도 동작하게 //setter를 이용해 의존관계 주입
-    public void setMemberRepository(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
-    @Autowired //setter를 이용해 의존관계 주입
-    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-        this.discountPolicy = discountPolicy;
-    } */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
