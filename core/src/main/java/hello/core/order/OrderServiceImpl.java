6e7d4f8eec21a136ff6276537a415211ea1dafab
leedongyeop <ldy_1204@naver.com>
2022-12-08 13:38:38 +0900

    다양한 의존관계 주입 방법

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -9,6 +9,12 @@ import org.springframework.stereotype.Component;
 @Component
 public class OrderServiceImpl implements OrderService {
 
+    /*
+    @Autowired private MemberRepository memberRepository;
+    필드로 의존관계 주입 -> 비추천 방식. 외부에서 테스트하기가 힘듬.
+    + NPE로 인해 결국 setter를 만들게 함 -> 이럴거면 setter injection이 낫다.
+     */
+
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
@@ -17,6 +23,16 @@ public class OrderServiceImpl implements OrderService {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+
+    /* @Autowired(required = false) //주입할 대상이 없어도 동작하게 //setter를 이용해 의존관계 주입
+    public void setMemberRepository(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Autowired //setter를 이용해 의존관계 주입
+    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+        this.discountPolicy = discountPolicy;
+    } */
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
